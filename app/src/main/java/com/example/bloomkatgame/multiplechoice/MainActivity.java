e80bcc888b210b4378a1cd44c1a410f5b66df71d
Shirley Shum <shirlshum@gmail.com>
2014-09-06 20:58:04 -0400

    Push from Android Studio

--- app/src/main/java/com/example/bloomkatgame/multiplechoice/MainActivity.java
+++ app/src/main/java/com/example/bloomkatgame/multiplechoice/MainActivity.java
@@ -2,18 +2,129 @@ package com.example.bloomkatgame.multiplechoice;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String TAG = "MainActivity";
+
+    //Declaring button variables
+    Button choiceButton1;
+    Button choiceButton2;
+    Button choiceButton3;
+    Button choiceButton4;
+
+    TextView imageQuestion;
+
+    TrueFalseChoices[] answerKey = new TrueFalseChoices[]{
+            new TrueFalseChoices(R.string.question_1, true),
+            new TrueFalseChoices(R.string.question_2, true),
+            new TrueFalseChoices(R.string.question_3, true),
+            new TrueFalseChoices(R.string.question_4, true),
+            new TrueFalseChoices(R.string.question_5, true)
+    };
+
+    //update imageQuestion
+    int currentIndex = 0;
+
+    private void updateQuestion(){
+        int question = answerKey[currentIndex].getImageQuestion();
+        imageQuestion.setText(question);
+    }
+
+    //Check the answer of the imageQuestion
+    private void checkAnswer(boolean userPressedTrue){
+        boolean answerIsTrue = answerKey[currentIndex].isTrueQuestion();
+
+        int imageResId = 0;
+
+        if(userPressedTrue == answerIsTrue){
+            imageResId = R.string.correct_toast;
+        }else{
+            imageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, imageResId, Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate() called");
+        setContentView(R.layout.activity_game);
+
+        //wiring the buttons
+        choiceButton1 = (Button)findViewById(R.id.choice_button1);
+        choiceButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+                updateQuestion();
+            }
+        });
+
+        choiceButton2 = (Button)findViewById(R.id.choice_button2);
+        choiceButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+                updateQuestion();
+            }
+        });
+        choiceButton3 = (Button)findViewById(R.id.choice_button3);
+        choiceButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(true);
+                updateQuestion();
+            }
+        });
+
+        choiceButton4 = (Button)findViewById(R.id.choice_button4);
+        choiceButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswer(false);
+                updateQuestion();
+            }
+        });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
